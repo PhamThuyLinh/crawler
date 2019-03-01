04752f9d6efe973b93961a9c4f05c2940524d768
PhamThuyLinh <phamthithuylinh30111997@gmail.com>
2019-03-01 13:46:25 +0700

    Them location

--- cafeland.php
+++ cafeland.php
@@ -5,36 +5,37 @@ $html = file_get_html("http://cafeland.vn/du-an/dat-chia-lo-tai-da-nang/");
 
 $tins = $html->find("div.page-content div.box-content ul.list-type-14 li");
 
-//echo count($tins);
+echo count($tins);
+echo "<br />";
 foreach ($tins as $t) {
 	$img = $t->find("a.wrap-img img",0)->src;
-	// echo "<img src='$img'/>";
+	//"<img src='$img'/>";
 
 	$title = $t->find("h3 a",0)->innertext;
 
-	$time = $t->find("span.news-date",0)->innertext;
-
 	$price = $t->find("span b",0)->innertext;
 
-	$mota = $t->find("p",0)->innertext;
+	$location = $t->find("div.box-type-2 a",0)->href;
 
-	$khuvuc = $t->find("div.box-type-2 a",0)->href;
+	$desc = $t->find("p",0)->innertext;
 
-	echo $link = $t->find("h3 a",0)->href;
+	$time = $t->find("span.news-date",0)->innertext;
 
-	echo "<hr />";
+	$link = $t->find("h3 a",0)->href;
+
+	// echo "<hr />";
 	$u = 'cafeland/'.basename($img);
 	file_put_contents($u, file_get_contents($img));
 	$tenFile = basename($img);
 	$title = htmlentities($title, ENT_QUOTES, "UTF-8");
-	$desc = htmlentities($mota, ENT_QUOTES,"UTF-8");
 	$price = htmlentities($price, ENT_QUOTES, "UTF-8");
-	$area = htmlentities($khuvuc, ENT_QUOTES, "UTF-8");
+	$location = htmlentities($location, ENT_QUOTES, "UTF-8");
+	$desc = htmlentities($desc, ENT_QUOTES,"UTF-8");
 	$time = htmlentities($time, ENT_QUOTES, "UTF-8");
 	$link = htmlentities($link, ENT_QUOTES, "UTF-8");
 
-
-	$qr ="INSERT INTO test(title, description, price, image, create_day, link, area) VALUES ('$title','$desc','$price','$tenFile','$time','$link','$area')";
+	
+	$qr ="INSERT INTO test(title, description, price, image, create_day, link, area, location) VALUES ('$title','$desc','$price','$tenFile','$time','$link',null,'$location')";
 
  	$result2 = mysqli_query($mysqli, $qr);
 }
--- muabannhadat.php
+++ muabannhadat.php
@@ -1,32 +1,51 @@
 <?php
-require "simple_html_dom.php";
 require "dbCon.php";
+require "simple_html_dom.php";
 
 $html = file_get_html("http://www.muabannhadat.vn/dat-ban-3515/tp-da-nang-s31?sf=dpo&so=d&p=0");
 
-$tins = $html->find("div.body-content div.container div.row div.container div.row div.col-xs-12 div.pull-left div.list-group div.list-group-item ");
+$tins = $html->find("div.body-content div.container div.row div.container div.row div.col-xs-12 div.pull-left div.list-group div.mbnd-item ");
 
 echo count($tins);
-echo "<hr />";
+// echo "<hr />";
 foreach ($tins as $t) {
-	// echo $title = $t->find("div.row div.resultItem div.col-xs-12 div.row div.col-md-9 a.title-filter-link",0)->innertext;
+	$title = $t->find("div.mbnd-item-body div.row div.col-lg-12 a h2.mbnd-item-title",0)->innertext;
 
-	// echo $dientich = $t->find("div.row div.resultItem div.col-xs-12 div.row",2)->find("div.col-xs-12",0)->plaintext;
+	$area = $t->find("div.mbnd-item-body div.row",3)->find("div.col-xs-3 p.mbnd-item-text",0)->plaintext;
 
-	// echo $price = $t->find("div.row div.resultItem div.col-xs-12 div.row div.col-md-3",0)->innertext;
+	$price = $t->find("div.mbnd-item-body div.row",3)->find("div.col-xs-5 p.mbnd-item-price",0)->innertext;
 
-	// echo $area = $t->find("div.row div.resultItem div.col-xs-12 div.row",1)->find("div.col-md-3",0)->innertext;
+	$street = $t->find("div.mbnd-item-body div.row",1)->find("div.col-lg-8 p.mbnd-item-text",0)->innertext;
 
-	// echo $desc =$t->find("div.row div.resultItem div.col-xs-12 div.row",3)->find("div.col-xs-12",0)->innertext;
-
-	// echo $time = $t->find("div.row div.resultItem div.col-xs-12 div.row",4)->find("div.col-lg-4",0)->innertext;
-
-	// echo $link = $t->find("div.row div.resultItem div.col-xs-12 div.row div.col-md-9 a.title-filter-link",0)->href;
-	// echo "<hr />";
-	// echo $link1 = "<span>http://www.muabannhadat.vn</span>".$link;
+	$district = $t->find("div.mbnd-item-body div.row",1)->find("div.col-lg-4 p.mbnd-item-text",0)->innertext;
 	
-	echo "<hr />";
+	$location = $street." ".$district;
+	
+	$desc =$t->find("div.mbnd-item-body div.row",2)->find("div.col-lg-12 p.mbnd-item-text",0)->innertext;
 
+	$time = $t->find("div.mbnd-item-body div.row",3)->find("div.col-xs-5 p.mbnd-item-text",0)->innertext;
+
+	$link = $t->find("div.mbnd-item-body div.row div.col-lg-12 a",0)->href;
+	//echo "<hr />";
+	$link1 = "<span>http://www.muabannhadat.vn</span>".$link;
+	
+	// echo "<hr />";
+	// //$u = 'cafeland/'.basename($img);
+	// //file_put_contents($u, file_get_contents($img));
+	// //$tenFile = basename($img);
+	$title = htmlentities($title, ENT_QUOTES, "UTF-8");
+	$area = htmlentities($area, ENT_QUOTES,"UTF-8");
+	$price = htmlentities($price, ENT_QUOTES, "UTF-8");
+	$location = htmlentities($location, ENT_QUOTES, "UTF-8");
+	$desc = htmlentities($desc, ENT_QUOTES,"UTF-8");
+	$time = htmlentities($time, ENT_QUOTES, "UTF-8");
+	$link1 = htmlentities($link1, ENT_QUOTES, "UTF-8");
+
+	echo "<br />";
+
+	$qr1 ="INSERT INTO test(title, description, price, image, create_day, link, area, location) VALUES ('$title','$desc','$price',null,'$time','$link1','$area','$location')";
+
+ 	$result2 = mysqli_query($mysqli, $qr1);
 
 }
 ?>
